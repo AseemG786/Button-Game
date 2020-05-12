66469b8e95b1fe041ca1deee9fc7558321afa41e
ag597482 <ag597482@gmai.com>
2020-05-12 17:14:34 +0530

    button colour added with transistion

--- back.js
+++ back.js
@@ -1,9 +1,30 @@
+var c=1;
+var sc=-1;
+function callfun(atr){
 
-var c=0;
-function fun(){
-b1.disabled=true;
-c++;
+sc++;
+//b1.disabled=true;
+
+var n=Math.random()*10;
+n=Math.floor(n);
+n=n*8/10;
+n=Math.floor(n);
+n++;
+c=n;
 var id="b"+c;
-document.getElementById(id).innerHTML=c;
 
+document. getElementById(atr). style. backgroundColor = '';
+document. getElementById(id). style. backgroundColor = 'Red';
+
+document.getElementById("tt").value=n;
+document.getElementById("score").value=sc;
+document.getElementById(id).innerHTML=n;
+
+
+
+
+}
+function fun()
+{
+alert("your score is : "+sc)
 }
