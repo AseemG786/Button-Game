9b8e488b19e6568c1f73c8fb45a0ea6594cf7f81
Aseem_Gupta <aseemg786@gmail.com>
2020-05-12 20:15:20 +0530

    added timer

---
+++ script.js
@@ -0,0 +1,117 @@
+//Define vars to hold time values
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+//Define vars to hold "display" value
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+
+//Define var to hold setInterval() function
+let interval = null;
+
+//Define var to hold stopwatch status
+let status = "stopped";
+
+//Stopwatch function (logic to determine when to increment next value, etc.)
+function stopWatch(){
+
+    seconds++;
+
+    //Logic to determine when to increment next value
+    if(seconds / 60 === 1){
+        seconds = 0;
+        minutes++;
+
+        if(minutes / 60 === 1){
+            minutes = 0;
+            hours++;
+        }
+
+    }
+
+    //If seconds/minutes/hours are only one digit, add a leading 0 to the value
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
+        displaySeconds = seconds;
+    }
+
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    }
+    else{
+        displayMinutes = minutes;
+    }
+
+    if(hours < 10){
+        displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours;
+    }
+
+    //Display updated time values to user
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+
+}
+
+
+
+function startStop(){
+
+    if(status === "stopped"){
+
+        //Start the stopwatch (by calling the setInterval() function)
+        interval = window.setInterval(stopWatch, 1000);
+        document.getElementById("startStop").innerHTML = "Stop";
+        status = "started";
+
+    }
+    else{
+
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "Start";
+        status = "stopped";
+
+    }
+
+}
+
+//Function to reset the stopwatch
+function reset(){
+
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById("display").innerHTML = "00:00:00";
+    document.getElementById("startStop").innerHTML = "Start";
+    document.getElementById("lap1").innerHTML = "Lap 1";
+    document.getElementById("lap2").innerHTML = "Lap 2";
+    document.getElementById("lap3").innerHTML = "Lap 3";
+
+}
+// to creat button
+
+//Function for Lap
+function lap(){
+
+    document.getElementById("lap1").innerHTML =seconds ;
+    // document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+ 
+}
+function lap2(){
+
+    document.getElementById("lap2").innerHTML =seconds ;
+    // document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+ 
+}
+function lap3(){
+
+    document.getElementById("lap3").innerHTML =seconds ;
+    // document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+ 
+}
